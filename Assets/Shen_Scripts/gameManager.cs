4e6941dfbecdbc1cbb3821035f031e0d2c7cb10a
Shen-2102687 <100612118+Shen-2102687@users.noreply.github.com>
2022-08-21 15:32:29 +0200

    added click functionality to blocks

---
+++ Assets/Shen_Scripts/gameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gameManager : MonoBehaviour
+{
+    public GameObject[] puzzleBlocks;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void clickFunction(GameObject puzzleBlock)
+    {
+        Debug.Log("Clicked image");
+        Debug.Log("Clicked puzzblock:" + puzzleBlock.gameObject.name);
+    }
+
+}
